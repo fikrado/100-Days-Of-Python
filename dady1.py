01e1d24598d6de419483331fbb58e73a7eb704ef
Yahye Abdirahman <68783841+fikrado@users.noreply.github.com>
2022-01-22 13:00:57 +0300

    day1

---
+++ dady1.py
@@ -0,0 +1,12 @@
+
+#1. Create a greeting for your program.
+print("hi this is band name genearator")
+
+#2. Ask the user for the city that they grew up in.
+a = input("enter a city that you love eg: hargisa\n ")
+
+#3. Ask the user for the name of a pet.
+b = input('what is your pet name\n ')
+
+#4. Combine the name of their city and pet and show them their band name.
+print(a + b)
