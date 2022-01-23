7b3c895ea45b8badac0cf0a9b793fc4a3a5ce25c
Yahye Abdirahman <68783841+fikrado@users.noreply.github.com>
2022-01-23 17:55:10 +0300

    Create day3.py
    day3

---
+++ day3.py
@@ -0,0 +1,42 @@
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#If the number can be divided by 2 with 0 remainder.
+if number % 2 == 0:
+  print("This is an even number.")
+#Otherwise (number cannot be divided by 2 with 0 remainder).
+else:
+  print("This is an odd number.")
+  
+  
+# 🚨 Don't change the code below 👇
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+combined_names = name1 + name2
+lower_names = combined_names.lower()
+t = lower_names.count("t")
+r = lower_names.count("r")
+u = lower_names.count("u")
+e = lower_names.count("e")
+first_digit = t + r + u + e
+
+l = lower_names.count("l")
+o = lower_names.count("o")
+v = lower_names.count("v")
+e = lower_names.count("e")
+second_digit = l + o + v + e
+
+score = int(str(first_digit) + str(second_digit))
+
+if (score < 10) or (score > 90):
+  print(f"Your score is {score}, you go together like coke and mentos.")
+elif (score >= 40) and (score <= 50):
+  print(f"Your score is {score}, you are alright together.")
+else:
+  print(f"Your score is {score}.")
