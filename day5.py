7dc724a7ee19d45994b4372600e87eb8834b2da2
Yahye Abdirahman <68783841+fikrado@users.noreply.github.com>
2022-01-25 08:39:36 +0000

    er

--- day5.py
+++ day5.py
@@ -8,3 +8,18 @@ for n in range(0, len(student_heights)):
 #Write your code below this row 👇
 print(sum(student_heights) / len(student_heights))
 
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+higher = 0
+
+for socore in student_scores:
+    if socore > higher:
+        higher = socore
+
+print(f"the highest nimo score is {higher}")
