963d9a5172573ba1efceb0a009238a6fb096b47e
Yahye Abdirahman <68783841+fikrado@users.noreply.github.com>
2022-01-25 11:25:57 +0300

    Create day5.py
    day5

---
+++ day5.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+
+#Write your code below this row 👇
+print(sum(student_heights) / len(student_heights))
+
