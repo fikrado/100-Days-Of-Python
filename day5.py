74522b38371940f2390f6addb22a844111dca507
Yahye Abdirahman <68783841+fikrado@users.noreply.github.com>
2022-01-25 11:08:54 +0000

    day 5 complate

--- day5.py
+++ day5.py
@@ -1,25 +1,67 @@
 # 🚨 Don't change the code below 👇
-student_heights = input("Input a list of student heights ").split()
-for n in range(0, len(student_heights)):
-  student_heights[n] = int(student_heights[n])
+# student_heights = input("Input a list of student heights ").split()
+# for n in range(0, len(student_heights)):
+#   student_heights[n] = int(student_heights[n])
 # 🚨 Don't change the code above 👆
 
 
-#Write your code below this row 👇
-print(sum(student_heights) / len(student_heights))
+# Write your code below this row 👇
+# print(sum(student_heights) / len(student_heights))
 
 # 🚨 Don't change the code below 👇
-student_scores = input("Input a list of student scores ").split()
-for n in range(0, len(student_scores)):
-  student_scores[n] = int(student_scores[n])
-print(student_scores)
+# student_scores = input("Input a list of student scores ").split()
+# for n in range(0, len(student_scores)):
+#   student_scores[n] = int(student_scores[n])
+# print(student_scores)
 # 🚨 Don't change the code above 👆
 
-#Write your code below this row 👇
-higher = 0
+# Write your code below this row 👇
+# higher = 0
 
-for socore in student_scores:
-    if socore > higher:
-        higher = socore
+# for socore in student_scores:
+#     if socore > higher:
+#         higher = socore
 
-print(f"the highest nimo score is {higher}")
+# print(f"the highest nimo score is {higher}")
+#even_sum = 0
+#for number in range(2, 101, 2):
+  # print(number)
+#  even_sum += number
+#print(even_sum)
+
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91 python day5.py
+
+for char in range(1, nr_letters + 1):
+  password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+  password_list += random.choice(symbols)
+
+for char in range(1, nr_numbers + 1):
+  password_list += random.choice(numbers)
+
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Your password is: {password}")
+
+
+
+
