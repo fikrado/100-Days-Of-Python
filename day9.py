0c72120b41094cfecc7e86b7c15e9adfebe1945b
Yahye Abdirahman <68783841+fikrado@users.noreply.github.com>
2022-01-31 17:59:29 +0000

    yep its me

--- day9.py
+++ day9.py
@@ -1,2 +1,24 @@
 from replit import clear
+print("stupid hame")
 
+bids = {}
+bidiing = False
+
+hb = 0
+
+def heigest_bifing(bir):
+  for i in bir:
+    amount = bir[i]
+    if amount > hb:
+      hb = amount
+
+
+while not bidiing:
+  name = input("what is your name ")
+  price = input("whats yout bit ")
+  bids[name] = price
+  contiune = input("are ther new biddings")
+  if contiune == "no":
+    bidiing = True
+  elif contiune == "yes":
+    clear()
