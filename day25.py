21b3f87c037858ca9bb0f03ac81c708e9c0cf483
Yahye Abdirahman <68783841+fikrado@users.noreply.github.com>
2022-02-25 21:59:09 +0300

    panda🐼 is king of data

--- day25.py
+++ day25.py
@@ -1,1 +1,21 @@
+import pandas as pd
+
+data = pd.read_csv("squarl.csv")
+sq = len(data[data["Primary Fur Color"] == "Gray"])
+print(sq)
+sq1 = len(data[data["Primary Fur Color"] == "Cinnamon"])
+print(sq1)
+sq2 = len(data[data["Primary Fur Color"] == "Black"])
+print(sq2)
+
+data_dic = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [sq, sq1, sq2]
+
+}
+df = pd.DataFrame(data_dic)
+df.to_csv("new.csv")
+
+
+
 
